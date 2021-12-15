514c0e8107e44ce57b69224670afb8957dfb68ef
GoodGuyGregory <greg.witt625@gmail.com>
2021-12-15 11:43:35 -0500

    adds dependency ignores and post method

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Middleware configurations:
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.send("Welcome to your App!");
@@ -21,3 +26,17 @@ app.get("/users", (req, res) => {
             res.json("error occured!");
         });
 });
+
+// POST Requests:
+app.post("/getUserById", (req, res) => {
+    if (!req.body.id) {
+        res.json("No ID found in reqest body.")
+    } else {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${req.body.id}`)
+            .then((response) => {
+                res.json(response.data)
+            }).catch(function (error) {
+                res.json("Error occured!")
+            })
+    }
+})
